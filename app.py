23e83630a5b53b68d025854c8661a6b286d8ce23
Ryojikn <ryojikn@gmail.com>
2021-05-11 10:03:25 -0300

    Alterando app.py

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ import soundfile as sf
 import wave
 import torch
 from werkzeug.datastructures import FileStorage
-from transformers import Wav2Vec2ForMaskedLM, Wav2Vec2Tokenizer
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
 import io
+from pprint import pprint
+
+vad_model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                              model='silero_vad')
+
+(get_speech_ts,
+ _, read_audio,
+ _, _, _) = utils
 
 # load pretrained model
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
-model = Wav2Vec2ForMaskedLM.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
 
 
 app = Flask(__name__)
@@ -26,23 +34,26 @@ def hello():
 
 def postJsonHandler():
 	vad = False
-	speakerlist = ['Speaker']
 	transcriptionlist = []
 	file = request.files['file']
 
 	with open("audio.wav", "wb") as aud:
 		aud_stream = file.read()
 		aud.write(aud_stream)
-	audio_input, sr = librosa.load('audio.wav', sr=16000)
-	
-	input_values = tokenizer(audio_input, return_tensors="pt").input_values
-	logits = model(input_values).logits
-	predicted_ids = torch.argmax(logits, dim=-1)
-	transcription = tokenizer.batch_decode(predicted_ids)[0]
 
-	transcriptionlist.append(transcription)
-	response = {'speakers': speakerlist, 'transcriptions': transcriptionlist}
+	audio_input, sr = librosa.load('audio.wav', sr=16000)
+	segments = get_speech_ts(read_audio('audio.wav'), model=vad_model, num_steps=1)
+	audio_segments = [audio_input[segment['start']:segment['end']] for segment in segments]
+	
+	for audio_seg in audio_segments:
+		input_values = tokenizer(audio_seg, return_tensors="pt").input_values
+		logits = model(input_values).logits
+		predicted_ids = torch.argmax(logits, dim=-1)
+		transcription = tokenizer.batch_decode(predicted_ids)[0]
+		transcriptionlist.append(transcription)
+
+	response = {'speakers': ['Speaker']*len(transcriptionlist), 'transcriptions': transcriptionlist}
 	return response
 
 if __name__ == '__main__':
-	app.run(port=33507)
+	app.run()
